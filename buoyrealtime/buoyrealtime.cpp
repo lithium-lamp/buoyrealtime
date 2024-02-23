8e1d0bb02d7dc23f5fc6769b9e477475ff5cc1c1
Hugo H <hughu123@student.liu.se>
2024-02-23 12:42:06 +0100

    change to vicctor's cpp file

--- buoyrealtime/buoyrealtime.cpp
+++ buoyrealtime/buoyrealtime.cpp
@@ -11,21 +11,21 @@
 
 int main()
 {
-    const float m = 1.f, k = 0.01f, r = 0.5f, rho = 1.f, g = 9.82f, phi = 2.f * 3.14f, A = 2.f/10.f, o = 3.f, p = 1.f;// consts
-    const std::vector<float> constvec { m, k, r, rho, g, phi, A, o, p };
+    const float m = 1.f, k = 0.8f, r = 0.5f, rho = 1.f, g = 9.82f, phi = 2.f * 3.14f, A = 2.f / 10.f, o = 3.f, p = 1.f;// consts
+    const std::vector<float> constvec{ m, k, r, rho, g, phi, A, o, p };
 
     float theta = 0.f, dL = 2.f, vx = 0.f, vy = 0.f;//variables
     std::vector<float> varvec{ theta, dL, vx, vy };
 
     // create the window
-    const float window_width = 1080.f;
-    const float window_height = 1080.f;
+    const float window_width = 800.f;
+    const float window_height = window_width;
 
-    const float window_width_half = window_width/2.f;
-    const float window_height_half = window_height/2.f;
+    const float window_width_half = window_width / 2.f;
+    const float window_height_half = window_height / 2.f;
 
-    float scale_constant = 35.f;
-    float spring_initial_position = 5.f * scale_constant;
+    float scale_constant = window_width / 40.f;
+    float spring_initial_position = 10.f * scale_constant;
 
     float x_coords = 0.f;
     float y_coords = o;
@@ -37,7 +37,7 @@ int main()
     const float buoy_diameter = 2 * r * scale_constant;
 
     sf::CircleShape buoy(buoy_diameter); //buoy
-    buoy.setFillColor(sf::Color(255,0,0));
+    buoy.setFillColor(sf::Color(255, 0, 0));
     buoy.setPosition(window_width_half - buoy_diameter + x_coords, window_height_half - buoy_diameter - y_coords);
 
     sf::RectangleShape wave(sf::Vector2f(window_width, 3.f)); //wave
@@ -69,7 +69,7 @@ int main()
 
     window.setFramerateLimit(60);
 
-    int frame = 0; 
+    int frame = 0;
 
     // run the program as long as the window is open
     while (window.isOpen()) {
@@ -85,40 +85,28 @@ int main()
         // clear the window with white color
         window.clear(sf::Color::White);
 
-        //
-
 
         coords pos = step(constvec, varvec, h, frame);
 
         x_coords = pos.x_pos;
 
         y_coords = pos.y_pos;
-        
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
                 float x_mouse = float(event.mouseButton.x);
                 float y_mouse = float(event.mouseButton.y);
-                
+
                 x_coords = x_mouse - window_width_half;
                 y_coords = window_height_half - y_mouse;
 
-
-                float x_diff = (x_mouse - window_width_half); //window_width_half, 
-                float y_diff = (y_mouse - window_height_half + spring_initial_position); //window_height_half + spring_initial_position
-
-                x_diff /= scale_constant;
-                y_diff /= scale_constant;
-
-                varvec[0] = std::atan2(x_diff, y_diff);
-                varvec[1] = std::sqrt(std::pow(x_diff, 2.f) + std::pow(y_diff, 2.f)) - p; //dL
+                varvec[0] = std::atan2(x_coords, y_coords);
+                varvec[1] = (std::sqrt(std::pow(x_coords, 2.f) + std::pow(y_coords, 2.f)) - p);
 
                 varvec[2] = 0.f;
                 varvec[3] = 0.f;
-
-                std::printf("x_coords: %f y_coords: %f\n ", x_coords, y_coords);
-
-
+                //std::cout << "x_diff: " << x_coords << " y_diff: " << y_coords << std::endl;
             }
 
             // "close requested" event: we close the window
@@ -126,12 +114,9 @@ int main()
                 window.close();
         }
 
-        
-
-
         buoy.setPosition(window_width_half - buoy_diameter + x_coords, window_height_half - buoy_diameter - y_coords);
 
-        force_x.setSize(sf::Vector2f(pos.x_force,3.f));
+        force_x.setSize(sf::Vector2f(pos.x_force, 3.f));
         force_y.setSize(sf::Vector2f(3.f, -1 * pos.y_force));
 
         force_x.setPosition(window_width_half + x_coords, window_height_half - y_coords);
